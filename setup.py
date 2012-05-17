303841710cd0072f7cf111c7e11a74b61a36857e
Wouter Bolsterlee <uws@xs4all.nl>
2012-05-17 14:43:27 +0200

    Make aaargh a proper Python distribution

---
+++ setup.py
@@ -0,0 +1,24 @@
+from os.path import join, dirname
+from setuptools import setup
+
+import aaargh
+
+setup(name='aaargh',
+      version=aaargh.__version__,
+      description='An arguably awesome application argument helper',
+      long_description=open(join(dirname(__file__), 'README.rst')).read(),
+      author='Wouter Bolsterlee',
+      author_email='uws@xs4all.nl',
+      url='https://github.com/wbolster/aaargh',
+      py_modules=['aaargh'],
+      install_requires=['argparse'],
+      license='BSD',
+      classifiers=('Development status :: 4 - Beta',
+                   'Environment :: Console',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 2.6',
+                   'Programming Language :: Python :: 2.7',
+                  )
+      )
