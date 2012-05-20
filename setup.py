3ed13f60808290cf700f3a4c710966bf9c506e61
Wouter Bolsterlee <uws@xs4all.nl>
2012-05-20 20:10:05 +0200

    List Python 3 support in classifier list in setup.py

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup(name='aaargh',
           'Programming Language :: Python :: 2',
           'Programming Language :: Python :: 2.6',
           'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
           'Topic :: Software Development :: Libraries :: Python Modules',
           'Topic :: Software Development :: User Interfaces',
           )
