626bd879b987984147ac26ecfd5cbec94751ae29
Wouter Bolsterlee <uws@xs4all.nl>
2014-02-16 20:39:13 +0100

    No longer use pbr for packaging (issue #12)

--- setup.py
+++ setup.py
@@ -1,6 +1,27 @@
-from setuptools import setup
+from os.path import join, dirname
+from setuptools import setup, find_packages
 
 setup(
-    setup_requires=['pbr'],
-    pbr=True,
+    name='aaargh',
+    version='0.6a',
+    description='An astonishingly awesome application argument helper',
+    long_description=open(join(dirname(__file__), 'README.rst')).read(),
+    author='Wouter Bolsterlee',
+    author_email='uws@xs4all.nl',
+    url='https://github.com/wbolster/aaargh',
+    packages=find_packages(),
+    install_requires=['argparse'],
+    license='BSD',
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: User Interfaces',
+    ),
 )
