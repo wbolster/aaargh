c09e1d80c05e09e0b3d669e5616d1208cd8bb099
Wouter Bolsterlee <uws@xs4all.nl>
2014-02-16 20:52:26 +0100

    Bump version to 0.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aaargh',
-    version='0.6a',
+    version='0.6',
     description='An astonishingly awesome application argument helper',
     long_description=open(join(dirname(__file__), 'README.rst')).read(),
     author='Wouter Bolsterlee',
